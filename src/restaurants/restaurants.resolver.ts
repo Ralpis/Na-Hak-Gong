612358fcc64e5438cd315c961367f310f0074003
Ralpis <rotjsdn1@naver.com>
2020-12-06 18:33:34 +0900

    새 파일:       src/chema.gql 	새 파일:       src/restaurants/entities/restaurant.entity.ts 	새 파일:       src/restaurants/restaurants.module.ts 	새 파일:       src/restaurants/restaurants.resolver.ts

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,10 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver(of => Restaurant)
+export class RestaurantResolver {
+  @Query(returns => Restaurant)
+  myRestaurnt() {
+    return true;
+  }
+}
