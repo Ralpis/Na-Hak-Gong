892fbc0416740c2c5c16a4426a54c37c8a52d92e
Ralpis <rotjsdn1@naver.com>
2020-12-07 21:27:15 +0900

    수정함:        src/restaurants/entities/restaurant.entity.ts 	수정함:        src/restaurants/restaurants.resolver.ts

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,11 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
-  @Query(returns => Restaurant)
-  myRestaurnt() {
-    return true;
+  @Query(returns => [Restaurant])
+  restaurnts(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
   }
 }
