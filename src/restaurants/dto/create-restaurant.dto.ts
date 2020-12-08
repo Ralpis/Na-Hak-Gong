e3420a11b05cbe29324adaddfc15f4f1b55f991f
Ralpis <rotjsdn1@naver.com>
2020-12-08 23:06:07 +0900

    수정함:        package.json 	수정함:        src/chema.gql 	수정함:        src/main.ts 	수정함:        src/restaurants/dto/create-restaurant.dto.ts 	수정함:        src/restaurants/restaurants.resolver.ts

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,16 +1,27 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
+/*
+ *** restaurant DTO
+ ***
+ ***
+ */
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(type => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
 
   @Field(type => Boolean)
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(type => String)
+  @IsString()
   address: string;
 
   @Field(type => String)
-  ownnerName: string;
+  @IsString()
+  ownerName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -16,3 +16,4 @@ export class RestaurantResolver {
     return true;
   }
 }
+
