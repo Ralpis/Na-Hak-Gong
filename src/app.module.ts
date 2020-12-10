af064620b3e2bb14a19ad3f7ab707bdb9bce3241
Ralpis <rotjsdn1@naver.com>
2020-12-10 22:45:29 +0900

    수정함:        package-lock.json 	수정함:        package.json 	수정함:        src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'a1',
+      password: '12345',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
+
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/chema.gql'),
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
