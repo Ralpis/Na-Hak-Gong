5be9fa13bbe5399d7d02ae7687b55cd9c28d516e
ralpi <rotjsdn1@naver.com>
2021-01-05 17:48:08 +0900

    mailing service revise

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,8 +2,7 @@ import got from 'got';
 import * as FormData from 'form-data';
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from '../common/common.constants';
-import { MailModuleOptions } from './mail.interfaces';
-import { Response } from 'express';
+import { EmailVars, MailModuleOptions } from './mail.interfaces';
 
 
 @Injectable()
@@ -11,18 +10,14 @@ export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {
-      this.sendEmail('testing','test').then(()=>{
-          console.log("Message sent");
-      }).catch((error)=>{
-          console.log(error.response.body);
-      });
   }
-  private async sendEmail(subject:string, content:string){
+  private async sendEmail(subject:string, template:string, emailVars: EmailVars[]){
       const form = new FormData();
-      form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+      form.append('from', `NA-HAK-GONG <mailgun@${this.options.domain}>`);
     form.append('to', `rotjsdn1@naver.com`);
-    form.append('subject', subject);
-    form.append('text', content);
+    form.append('subject', template);
+    form.append('template',`check`);
+    emailVars.forEach(eVar=>form.append(eVar.key, eVar.value));
 
     const response = await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`,{
         method:'POST',
