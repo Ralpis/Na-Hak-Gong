54fef1899d40d6176c2b93933c77a0695958938c
ralpi <rotjsdn1@naver.com>
2021-01-05 13:11:03 +0900

    modified:   src/app.module.ts 	new file:   src/common/common.constants.ts 	deleted:    src/jwt/jwt.constants.ts 	modified:   src/jwt/jwt.module.ts 	modified:   src/jwt/jwt.service.ts 	new file:   src/mail/mail.interfaces.ts 	modified:   src/mail/mail.module.ts

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,4 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
+import { CONFIG_OPTIONS } from '../common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
 
 @Module({})
-export class MailModule {}
+export class MailModule {
+    static forRoot(options: MailModuleOptions): DynamicModule {
+        return {
+          module: MailModule,
+          providers: [
+            {
+              provide: CONFIG_OPTIONS,
+              useValue: options,
+            },
+          ],
+          exports: [],
+        };
+      }
+
+}
