c3acb5aa627eec32c4542e32209138c6a6ff1c34
Ralpis <rotjsdn1@naver.com>
2021-01-27 17:57:43 +0900

    8.0 fix

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -2,6 +2,8 @@ import { Test } from '@nestjs/testing';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
 
+const TEST_KEY = 'testKey';
+
 describe('JwtService', () => {
   let service: JwtService;
   beforeEach(async () => {
@@ -10,7 +12,7 @@ describe('JwtService', () => {
         JwtService,
         {
           provide: CONFIG_OPTIONS,
-          useValue: { privateKey: 'testKey' },
+          useValue: { privateKey: TEST_KEY },
         },
       ],
     }).compile();
