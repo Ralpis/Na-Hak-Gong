491b089f8c1e7b6e3b3ce39127c9d7782d077fec
Ralpis <rotjsdn1@naver.com>
2021-01-29 14:56:51 +0900

    9.2 Testing createAccount part One

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+const GRAPHQL_ENDPOINT = '/graphql';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -19,11 +21,35 @@ describe('UserModule (e2e)', () => {
     await getConnection().dropDatabase();
     app.close();
   });
+  describe('createAccount', () => {
+    const EMAIL = 'he@he.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `mutation{
+          createAccount(input:{
+            email:"${EMAIL}"
+            password:"!23123",
+            role:Client
+          }){
+            ok
+            error
+          }
+        }`,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
 
-  it.todo('me');
+    it.todo('should fail if acconut already exists');
+  });
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
